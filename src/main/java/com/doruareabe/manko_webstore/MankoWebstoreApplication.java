e8a8e4c7a94a0bf9f9ef76c72296520b963a0477
Mykyta Ponomarenko <mykpon3769@gmail.com>
2020-10-16 14:31:45 +0200

    Initial commit

---
+++ src/main/java/com/doruareabe/manko_webstore/MankoWebstoreApplication.java
@@ -0,0 +1,13 @@
+package com.doruareabe.manko_webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MankoWebstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MankoWebstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doruareabe/manko_webstore/MankoWebstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doruareabe.manko_webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MankoWebstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
